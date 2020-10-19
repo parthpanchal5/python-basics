93f00a64be6174fc54cfcdfcbbe3d6ef4652d15a
parthpanchal5 <parthpanchal2468@gmail.com>
2020-10-19 19:21:07 -0400

    all

--- basics.py
+++ basics.py
@@ -1,7 +1,104 @@
-print('Hello World')
-myName = input("Whats your name?\n")
-print("It is good to meet you, " + myName)
-print('The length of your name is: ')
-print(len(myName))
-myAge = input("Whats age?\n")
-print('you will be ' + str(int(myAge) + 1) + ' in a year.')
+# Input Examples
+
+# print('Hello World')
+# myName = input("Whats your name?\n")
+# print("It is good to meet you, " + myName)
+# print('The length of your name is: ')
+# print(len(myName))
+# myAge = input("Whats age?\n")
+# print('you will be ' + str(int(myAge) + 1) + ' in a year.')
+
+# Flow Controls
+
+# example 1
+# name = 'Msry'
+# password = 'swordfish'
+# if name == 'Mary':
+#     print('Hello Mary')
+#     if password == 'swordfish':
+#         print('Access Granted')
+#     else:
+#         print('Wrong Pass')
+# else:
+#     print('Program Terminated!')
+
+# example 2
+# name = 'Alisce'
+# age = 11
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif age < 12:
+#     print('Not Allice')
+# elif age > 2000:
+#     print('you are immortal')
+# else:
+#     print('hello alice')
+
+# exxample 3
+# with if
+import sys
+import random
+spam = 0
+# if spam < 5:
+# 	print ('Hello')
+# 	spam = spam + 1
+
+# with while
+# while spam < 5:
+#     print('hello')
+#     spam = spam + 1
+
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+# print('thanks')
+
+# example 4
+# while True:
+#     print('Who are you?')
+#     name = input()
+#     if name != 'Parth':
+#         continue
+#     print('Hello ' + name + ' What\'s your password')
+#     password = input()
+#     if password == 'swordfish':
+#         break
+# print('Access Granted')
+
+#  example 5
+# for loop
+# print('My name is')
+# for i in range(5):
+#     print('Parth five times (' + str(i) + ')')
+
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print (total)
+
+#  normal for loop Start - Stop
+# for i in range(12, 16):
+
+# for loop with Start - Stop - Step
+# for i in range(0, 10, 2):
+
+# for loop to count down
+# for i in range(5, -1, -2):
+#     print(i)
+
+# importing modules
+
+# import random
+# for i in range(5):
+#     print(random.randint(1, 10))
+
+
+# while True:
+#     print('Type exit to exit.')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('You typed ' + response + '.')
+
+
