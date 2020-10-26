70bd433bdb075ef8e6fb27af39ebe48f7d469e54
parthpanchal5 <parthpanchal2468@gmail.com>
2020-10-26 19:04:10 -0400

    Completed till lists on idle now working on vsc

--- basics.py
+++ basics.py
@@ -38,6 +38,7 @@
 # with if
 import sys
 import random
+from typing import Type
 spam = 0
 # if spam < 5:
 # 	print ('Hello')
@@ -93,7 +94,7 @@ spam = 0
 # for i in range(5):
 #     print(random.randint(1, 10))
 
-
+# import sys
 # while True:
 #     print('Type exit to exit.')
 #     response = input()
@@ -101,4 +102,107 @@ spam = 0
 #         sys.exit()
 #     print('You typed ' + response + '.')
 
+# functions
+# def hello():
+# 	print('Howdy!')
+# 	print('Hwdy!')
+# 	print('Hello There')
 
+# hello()
+# hello()
+# hello()
+
+
+# with params
+# def hello(name):
+#     print('Hello ' + name)
+
+
+# hello('Alice')
+# hello('Bob')
+
+
+# def getAnswer(answerNum):
+#     if answerNum == 1:
+#         return 'It is certain'
+#     elif answerNum == 2:
+#         return 'It is decidedly so'
+#     elif answerNum == 3:
+#         return 'Yes'
+#     elif answerNum == 4:
+#         return 'Reply hazy try again'
+#     elif answerNum == 5:
+#         return 'Ask again later'
+#     elif answerNum == 6:
+#         return 'Concentrate and ask again'
+#     elif answerNum == 7:
+#         return 'My reply is no'
+#     elif answerNum == 8:
+#         return 'Outlook not so good'
+#     elif answerNum == 9:
+#         return 'very doubtful'
+
+
+# r = random.randint(1, 9)
+# print(r)
+# fortune = getAnswer(r)
+# print(fortune)
+
+# test = None
+# print(test)
+
+# Exception handling
+# def spam(divideBy):
+#     try:
+#         return 42 / divideBy
+#     except ZeroDivisionError:
+#         print('Error: Invalid Argument')
+
+
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+
+# Example: Guess my number game
+
+# secretNum = random.randint(1, 20)
+# print('I am thinking of a number between 1 and 20')
+
+# # Ask player to guess 6 times
+# for guessTaken in range(1, 7):
+#     guess = int(input('Take a guess: '))
+
+#     if guess < secretNum:
+#         print('Your guess is too low')
+#     elif guess > secretNum:
+#         print('Your guess is too high')
+#     else:
+#         break  # this codition is correct guess
+
+#     if guess == secretNum:
+#         print('Good job! You guessed my number in ' +
+#               str(guessTaken) + ' guesses!')
+#     else:
+#         print('Nope. the number i was thinking of was ' + str(secretNum))
+
+# Practice 1
+# def collatz(number):
+#     if number % 2 == 0:
+#         print(number // 2)
+#         return number // 2
+#     elif number % 2 == 1:
+#         result = 3 * number + 1
+#         print(result)
+#         return result
+
+
+# try:
+#     userInput = int(input("Enter Num: "))
+#     while (userInput != 1):
+#         userInput = collatz(int(userInput))
+# except ValueError:
+#     print("Please enter integer")
+
+# list
